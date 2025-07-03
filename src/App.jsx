c398ff4b7e6651ef4669e15e7325239a0edb3bc5
Golvi1124 <golvi1124@gmail.com>
2025-07-03 16:41:17 +0200

    Hardcoded version + styling

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalendarApp from "./components/CalendarApp.jsx";
+import "./components/CalendarApp.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalendarApp />
+    </div>
+  );
+};
+
+export default App;
